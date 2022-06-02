ad00480aa59ea14c0c05249a8af89a854f5717a1
洋平 山内 <y.yamauchi12078@gmail.com>
2022-06-02 23:13:16 +0900

    プロジェクト ファイルを追加します。

---
+++ Caluculator/Caluculator.h
@@ -0,0 +1,32 @@
+﻿
+// Caluculator.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CCaluculatorApp:
+// このクラスの実装については、Caluculator.cpp を参照してください
+//
+
+class CCaluculatorApp : public CWinApp
+{
+public:
+	CCaluculatorApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CCaluculatorApp theApp;
